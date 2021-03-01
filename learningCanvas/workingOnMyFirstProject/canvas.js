84fc3eeb7107690862dd488b9584030e7833685f
Snake-Eyes-9554 <adalloul-14@users.noreply.github.com>
2021-02-28 23:02:54 -0600

    Arranging things

---
+++ learningCanvas/workingOnMyFirstProject/canvas.js
@@ -0,0 +1,7 @@
+//Declaring variables:
+let canvas = document.querySelector("canvas");
+let c = canvas.getContext("2d");
+
+//Declaring starting conditions:
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
